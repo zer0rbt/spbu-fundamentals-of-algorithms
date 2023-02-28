3c102fc8e09524383d12c569cd847ee5067e2f48
Anton Pershin <tony.pershin@gmail.com>
2023-02-28 21:10:43 +0300

    Added practicum 2 and 3 templates

---
+++ practicum_2/dfs_graph_template.py
@@ -0,0 +1,43 @@
+import queue
+from typing import Any
+
+import networkx as nx
+
+from src.plotting import plot_graph
+
+
+def visit(node: Any):
+    print(f"Wow, it is {node} right here!")
+
+
+def dfs_iterative(G: nx.Graph, node: Any, visited: dict[Any]):
+    ##########################
+    ### PUT YOUR CODE HERE ###
+    ##########################
+    pass
+
+
+def topological_sort(G: nx.DiGraph, node: Any, visited: dict[Any]):
+    ##########################
+    ### PUT YOUR CODE HERE ###
+    ##########################
+    pass
+
+
+if __name__ == "__main__":
+    # Load and plot the graph
+    G = nx.read_edgelist("practicum_2/graph_2.edgelist", create_using=nx.Graph)
+    # plot_graph(G)
+
+    print("Iterative DFS")
+    print("-" * 32)
+    visited = {n: False for n in G}
+    dfs_iterative(G, node="0", visited=visited)
+    print()
+
+    G = nx.read_edgelist("practicum_2/graph_2.edgelist", create_using=nx.DiGraph)
+    plot_graph(G)
+    print("Topological sort")
+    print("-" * 32)
+    visited = {n: False for n in G}
+    topological_sort(G, node="0", visited=visited)
---
+++ practicum_3/sp_template.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+import networkx as nx
+
+from src.plotting import plot_graph
+
+
+def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
+    shortest_paths = {}  # key = destination node, value = list of intermediate nodes
+
+    ##########################
+    ### PUT YOUR CODE HERE ###
+    ##########################
+
+    return shortest_paths
+
+
+if __name__ == "__main__":
+    G = nx.read_edgelist("practicum_3/graph_1.edgelist", create_using=nx.Graph)
+    plot_graph(G)
+    shortest_paths = dijkstra_sp(G, source_node="0")
+    test_node = "5"
+    shortest_path_edges = [
+        (shortest_paths[test_node][i], shortest_paths[test_node][i + 1])
+        for i in range(len(shortest_paths[test_node]) - 1)
+    ]
+    plot_graph(G, highlighted_edges=shortest_path_edges)
