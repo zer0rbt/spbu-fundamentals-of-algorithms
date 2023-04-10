f5fb9f739483c0fe69485162b52306a834cb7f33
zer0rbt <fedorsh2004@gmail.com>
2023-04-10 20:19:20 +0300

    Add comments to the practicum 3 and 4

--- practicum_3/homework/mst.py
+++ practicum_3/homework/mst.py
@@ -6,32 +6,29 @@ import heapq as hq
 from src.plotting import plot_graph
 
 
-# Sorry for my poor English...
 def prim_mst(g: nx.Graph, start_node="0") -> set:
-    mst_set = set()  # set of nodes included into MST
-    rest_set = set(g.nodes())  # set of nodes not yet included into MST
-    mst_edges = set()  # set of edges constituting MST
+    mst_set = set()  # set of nodes included in MST
+    rest_set = set(g.nodes())  # set of nodes not yet included in MST
+    mst_edges = set()  # set of edges constituting the MST
     possible_edges_hq = []
     last_visited_node = start_node
 
     mst_set.add(start_node)
     rest_set.discard(start_node)
     while len(rest_set) > 1:
-        for node in g.neighbors(last_visited_node):  # Here we are adding edges from our last visited node.
-            # tuple (weight, edge) in order to use heapq
+        for node in g.neighbors(last_visited_node):  # Here we add edges from our last visited node.
+            # a tuple (weight, edge) to use heapq
             new_edge = (g.edges[last_visited_node, node]["weight"], last_visited_node, node)
 
-            # Note: we could also do not include duplicate edges into a possible_edges, by adding this condition:
-            # " ...and (reversed_edge[::-1] not in possible_edges) "
-            # but tests show that if we do so, speed decreases on 6000%! <2.4s from 0.04s on g=1000, e=10000>
+            # Note: we could also exclude duplicate edges from possible_edges by adding this condition:
+            # "...and (reversed_edge[::-1] not in possible_edges) "
+            # but tests show that doing so decreases speed by 6000%! <2.4s from 0.04s on g=1000, e=10000>
             if node not in mst_set:
                 hq.heappush(possible_edges_hq, new_edge)
 
         possible_edge = hq.heappop(possible_edges_hq)
 
-        # Here we are trying to find edge that would push our "frontline".
-        # Note: these two conditions can't be False at the same time,
-        #                                           because one of nodes is a last_visited_node and already in mst_set.
+        # Here we try to find an edge that would push our "frontline".
         while (possible_edge[1] in mst_set) and (possible_edge[2] in mst_set):
             possible_edge = hq.heappop(possible_edges_hq)
 
@@ -39,9 +36,9 @@ def prim_mst(g: nx.Graph, start_node="0") -> set:
             mst_edges.add(possible_edge[1:])
             rest_set.discard(last_visited_node)
 
-            # Here we are changing var"last_seen_node" to a new one; We have to use ternary operator because
-            # we don't know where is the new node: on [1] or [2].
-            # For ex.: l_v_n = 1 and possible_edge could be (1, v, w) as well as (u, 1, w).
+            # Here we change the variable "last_visited_node" to a new one. We use a ternary operator because
+            # we don't know where the new node is: on [1] or [2].
+            # For example: last_visited_node = N, and possible_edge could be (N, v, w) as well as (u, N, w).
             last_visited_node = possible_edge[1] if possible_edge[2] == last_visited_node else possible_edge[2]
 
             mst_set.add(last_visited_node)
--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -13,27 +13,33 @@ def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
     rest_set = set(G.nodes())  # set of nodes not yet included into sp
     possible_edges_hq = []  # priority queue (= heapq), that contains of (path_lenght, (from_node, to_node))
 
-    # I hope there would be some comments when you would read it...
+    # Starting node has a path to itself and a zero distance from itself
     shortest_paths[source_node] = [source_node]
-    hq.heappush(possible_edges_hq, (0, (source_node, source_node)))
+
     possible_edge = (0, (source_node, source_node))
     dist[source_node] = 0
+    hq.heappush(possible_edges_hq, possible_edge)
+
     while len(rest_set) > 0:
         edge_weight, edge = possible_edge
         cur_node = edge[1]
         rest_set.discard(cur_node)
 
+        # Look for the minimum-weight edge from cur_node to one of its neighbors
         for node in G.neighbors(cur_node):
             new_edge_weight = G.edges[cur_node, node]["weight"] + edge_weight
             if dist[node] > new_edge_weight:
+                # Update shortest path and distance to the current neighbor
                 shortest_paths[node] = shortest_paths[cur_node] + [node]
                 dist[node] = new_edge_weight
+                # Push the neighbor's edge to the priority queue
                 hq.heappush(possible_edges_hq, (new_edge_weight, (cur_node, node)))
 
+        # Find the minimum-weight edge in the priority queue that leads to a node not included in the shortest path yet
         while possible_edge[1][1] not in rest_set:
-            try:
+            if len(possible_edges_hq) > 0:
                 possible_edge = hq.heappop(possible_edges_hq)
-            except IndexError:
+            else:
                 return shortest_paths
 
     return shortest_paths
